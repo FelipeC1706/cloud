6441e94047f56df23a07f0c9c7ec9d9f4e74be30
FelipeC1706 <jcarvajal@uniempresarial.edu.co>
2023-09-26 03:55:23 -0500

    felipe

--- clases/conexion.php
+++ clases/conexion.php
@@ -1,18 +1,53 @@
 <?php 
- class Conexion extends PDO { 
-   private $tipo_de_base = 'mysql';
-   private $host = 'localhost';
-   private $nombre_de_base = 'parking';
-   private $usuario = 'root';
-   private $contrasena = ''; 
-   public function __construct() {
-      //Sobreescribo el método constructor de la clase PDO.
-      try{
-         parent::__construct("{$this->tipo_de_base}:dbname={$this->nombre_de_base};host={$this->host};charset=utf8", $this->usuario, $this->contrasena);
-      }catch(PDOException $e){
-         echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
+//  class Conexion extends PDO { 
+//    private $tipo_de_base = 'mysql';
+//    private $host = 'localhost';
+//    private $nombre_de_base = 'parking';
+//    private $usuario = 'root';
+//    private $contrasena = ''; 
+//    public function __construct() {
+//       //Sobreescribo el método constructor de la clase PDO.
+//       try{
+//          parent::__construct("{$this->tipo_de_base}:dbname={$this->nombre_de_base};host={$this->host};charset=utf8", $this->usuario, $this->contrasena);
+//       }catch(PDOException $e){
+//          echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
+//          exit;
+//       }
+//    } 
+//  } 
+
+// $pps = $con->getConexion()->prepare("SELECT *  FROM Persona");
+
+//       $pps->execute();
+
+//       echo json_encode(['Persona'=>$pps->fetchAll(PDO::FETCH_ASSOC)]);
+
+require_once 'setting.php';
+
+class conexion{
+   private $conector = null;
+
+   public function getConexion()
+   {
+
+         try{
+            $this->conector = new PDO("sqlsrv:server=".SERVIDOR.";database=".DATABASE, USUARIO, PASSWORD);
+
+            return $this->conector;
+         }
+         catch(PDOException $e){
+               echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
          exit;
-      }
-   } 
- } 
+         }
+
+         
+   }
+}
+
+   $con = new conexion();
+
+   if($con->getConexion() != null){
+      
+      echo "conexion Exitosa";
+   }
 ?>
---
+++ clases/setting.php
@@ -0,0 +1,9 @@
+<?php
+define("USUARIO", "SJP");
+
+define("PASSWORD", "FCC123456789.");
+
+define("DATABASE", "BD_parking");
+
+define("SERVIDOR", "parqueaderosjp.database.windows.net");
+?>
